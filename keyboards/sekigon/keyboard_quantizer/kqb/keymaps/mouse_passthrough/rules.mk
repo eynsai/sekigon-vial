54148f722f73e69d9375d47a40d65d2c7fe9d082
Morgan Sun <morgan.newell.sun@gmail.com>
2024-09-28 19:11:41 -0400

    remove mini

--- keyboards/sekigon/keyboard_quantizer/kqb/keymaps/mouse_passthrough/rules.mk
+++ keyboards/sekigon/keyboard_quantizer/kqb/keymaps/mouse_passthrough/rules.mk
@@ -3,9 +3,11 @@ VIAL_ENABLE = yes
 VIAL_INSECURE = no
 SRC += ../quantizer_mouse.c
 
+
+
+# eynsai
 USERSPACE_DIR = $(TOP_DIR)/../qmk_firmware/users/eynsai
 SRC += $(USERSPACE_DIR)/mouse_passthrough_sender.c
 VPATH += $(USERSPACE_DIR)
-
 LTO_ENABLE = yes
 OPT = 3  # -O3 optimization
